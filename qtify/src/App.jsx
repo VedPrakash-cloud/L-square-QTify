5c1b082b157ade8d0b099c4283ed7b775311cc29
VedPrakash-cloud <rocky007ved@gmail.com>
2025-06-22 19:27:46 +0530

    header update

--- qtify/src/App.jsx
+++ qtify/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
-import Logo from "./Logo/Logo.jsx"
+import Logo from "./Logo/Logo"
+import Hero from "./Hero/Hero"
+import Navbar from "./Navbar/Navbar"
+import Search from './Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <>
       <Logo />
+      <Hero />
+      <Navbar />
+      <Search />
+      </>
     </div>
   );
 }
