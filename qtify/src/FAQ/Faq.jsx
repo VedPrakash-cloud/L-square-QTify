a429eeee624815056d5f89ec1ce92e076ea3e07f
VedPrakash-cloud <rocky007ved@gmail.com>
2025-07-05 18:49:36 +0530

    FAQ page updater

--- qtify/src/FAQ/Faq.jsx
+++ qtify/src/FAQ/Faq.jsx
@@ -1,49 +1,60 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import style from "../FAQ/Faq.module.css"
+import style from "../FAQ/Faq.module.css";
 
 export default function AccordionUsage() {
+  const [data, setData] = useState([]);
+
+  const FetchFaq = async () => {
+    try {
+      const response = await axios.get(
+        "https://qtify-backend-labs.crio.do/faq"
+      );
+      setData(response.data.data);
+    } catch (err) {
+      console.error("unable to fetch FAQs", err);
+    }
+  };
+
+  useEffect(() => {
+    FetchFaq();
+  }, []);
+
   return (
     <div className={style.mainContainer}>
       <h1>FAQs</h1>
-      <Accordion sx={{marginBottom:"10px", borderRadius:"8px", border: "1px solid #FFFFFF"}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{color:"#34C94B", fontSize: 40}} />}
-          aria-controls="panel1-content"
-          id="panel1-header"
+      {data.map((items) => (
+        <Accordion
+          key={uuidv4()}
           sx={{
-            background:"#121212",
-            color:"#FFFFFF",
-            borderRadius: "6px"
+            marginBottom: "10px",
+            borderRadius: "8px",
+            border: "1px solid #FFFFFF",
           }}
         >
-          <Typography component="span">Is QTify free to use?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          Yes, QTify is free for everyone...
-        </AccordionDetails>
-      </Accordion>
-      <Accordion sx={{marginBottom:"10px", borderRadius:"8px", border: "1px solid #FFFFFF"}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{color:"#34C94B", fontSize: 40}}/>}
-          aria-controls="panel2-content"
-          id="panel2-header"
-          sx={{
-            background:"#121212",
-            color:"#FFFFFF",
-            borderRadius: "6px"
-          }}
-        >
-          <Typography component="span">Can I download and listen to songs offline?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          Sorry, unfortunately we don't provide the service to download any songs.
-        </AccordionDetails>
-      </Accordion>
+          <AccordionSummary
+            expandIcon={
+              <ExpandMoreIcon sx={{ color: "#34C94B", fontSize: 40 }} />
+            }
+            aria-controls="panel1-content"
+            id="panel1-header"
+            sx={{
+              background: "#121212",
+              color: "#FFFFFF",
+              borderRadius: "6px",
+            }}
+          >
+            <Typography component="span">{items.question}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>{items.answer}</AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
